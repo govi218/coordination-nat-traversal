500e317c9f2c6b92ceace76c9e533d8bc21b7123
Gov <info@govindmohan.com>
2021-06-24 18:32:58 -0400

    added getifaddrs

--- peer.cpp
+++ peer.cpp
@@ -1,6 +1,11 @@
 #include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <linux/if_link.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include <cstring>
 #include <iostream>
@@ -13,6 +18,62 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: ./peer [RECEIVER_IP]" << std::endl;
         return EXIT_FAILURE;
     }
+
+    struct ifaddrs* ifaddr;
+    int family, s;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Walk through linked list, maintaining head pointer so we
+       can free list later. */
+
+    for (struct ifaddrs* ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        family = ifa->ifa_addr->sa_family;
+
+        /* Display interface name and family (including symbolic
+           form of the latter for the common families). */
+
+        printf("%-8s %s (%d)\n", ifa->ifa_name,
+               (family == AF_PACKET)  ? "AF_PACKET"
+               : (family == AF_INET)  ? "AF_INET"
+               : (family == AF_INET6) ? "AF_INET6"
+                                      : "???",
+               family);
+
+        /* For an AF_INET* interface address, display the address. */
+
+        if (family == AF_INET || family == AF_INET6) {
+            s = getnameinfo(ifa->ifa_addr,
+                            (family == AF_INET) ? sizeof(struct sockaddr_in)
+                                                : sizeof(struct sockaddr_in6),
+                            host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+
+            printf("\t\taddress: <%s>\n", host);
+
+        } else if (family == AF_PACKET && ifa->ifa_data != NULL) {
+            struct rtnl_link_stats* stats =
+                static_cast<rtnl_link_stats*>(ifa->ifa_data);
+
+            printf(
+                "\t\ttx_packets = %10u; rx_packets = %10u\n"
+                "\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
+                stats->tx_packets, stats->rx_packets, stats->tx_bytes,
+                stats->rx_bytes);
+        }
+    }
+
+    freeifaddrs(ifaddr);
 
     std::string priv_rcvr = args.at(0);  // who we send to
     std::vector<char> spMsg(100);
